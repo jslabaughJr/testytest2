1556ffc1d74b9d1231218379944fc5e3dea00f2e
johnny <jslabauugh@gmail.com>
2023-05-10 14:14:33 -0400

    Initial Commit

---
+++ testytest2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  testytest2
+//
+//  Created by Johnny Slabaugh on 5/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testytest2/testytest2App.swift
@@ -0,0 +1,17 @@
+//
+//  testytest2App.swift
+//  testytest2
+//
+//  Created by Johnny Slabaugh on 5/10/23.
+//
+
+import SwiftUI
+
+@main
+struct testytest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
